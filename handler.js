03903ddbc907ff55f9f6e7a60659727c7aa291fb
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-11-07 14:06:27 +1100

    added slack test

--- handler.js
+++ handler.js
@@ -1,5 +1,32 @@
-"use strict";
+import { App } from "@slack/bolt";
 
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+export const hello = async (event, context) => {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+
+  app.event("message.im", ({ event, say }) => {
+    console.log("In here");
+    say(`Hello World, <@${event.user}>!`);
+  });
+
+  console.log("Received event:", JSON.stringify(event, null, 2));
+  const body = JSON.parse(event.body);
+  console.log(body);
+  const challenge = body.challenge;
+  console.log(challenge);
+
+  return {
+    statusCode: 200,
+    body: challenge,
+  };
+  // throw new Error('Something went wrong');
+};
+
+/*
 module.exports.default = async (event) => {
   return {
     statusCode: 200,
@@ -16,17 +43,4 @@ module.exports.default = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-
-module.exports.hello = async (event, context) => {
-  console.log("Received event:", JSON.stringify(event, null, 2));
-  const body = JSON.parse(event.body);
-  console.log(body);
-  const challenge = body.challenge;
-  console.log(challenge);
-
-  return {
-    statusCode: 200,
-    body: challenge,
-  };
-  // throw new Error('Something went wrong');
-};
+*/
