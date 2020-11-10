c6dfa7ea9c634190e0b16c4af342256fc5026a6e
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-11-10 20:07:49 +1100

    basic functioning web scraper slack news bot

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 import { App } from "@slack/bolt";
-import getFirstPost from "./scraper";
+import { getFirstPost } from "./scraper";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -8,15 +8,12 @@ const app = new App({
 
 async function publishMessage(id, text) {
   try {
-    // scrape the latest article
-    const article = await getFirstPost();
-
     // Call the chat.postMessage method using the built-in WebClient
     const result = await app.client.chat.postMessage({
       // The token you used to initialize your app
-      token: "xoxb-1466612478967-1466635297799-m5tlNijXFgaMRmnMnmGb0VQj",
+      token: null,
       channel: id,
-      text: article,
+      text: text,
       // You could also use a blocks[] array to send richer content
     });
 
@@ -28,42 +25,28 @@ async function publishMessage(id, text) {
 }
 
 export const hello = async (event, context) => {
-  getFirstPost();
   console.log("Received event:", JSON.stringify(event, null, 2));
   const body = JSON.parse(event.body);
   console.log("Event Body:", body);
   const slackE = body.event;
 
-  // run this when back home  `
-  console.log("app", app);
-
-  if (slackE.channel_type == "im" && slackE.type == "message") {
-    console.log("OOOOOOOOOOOOOOOOOOSI'm in here");
-    publishMessage(slackE.channel, `Hello World, <@${event.user}>!`);
+  // this isnt robust enough at the moment. I need it to handle non related commands and then end, rather than keep firing
+  if (
+    slackE.channel_type == "im" &&
+    slackE.type == "message" &&
+    slackE.text == "Hey this is a test"
+  ) {
+    // scrape the latest article
+    const article = await getFirstPost();
+    await publishMessage(slackE.channel, article);
+    return {
+      statusCode: 200,
+      body: slackE,
+    };
+  } else {
+    // need to implement a message that gives rules and ends, rather than throwing an error
+    //could have a trycatch here as well to handle overall error
+    throw Error("Something went wrong");
   }
-
-  return {
-    statusCode: 200,
-    body: slackE,
-  };
   // throw new Error('Something went wrong');
 };
-
-/*
-module.exports.default = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-*/
--- scraper.js
+++ scraper.js
@@ -1,19 +1,17 @@
-import { JSDOM } from "jsdom";
 import axios from "axios";
+import cheerio from "cheerio";
 
 export const getFirstPost = async () => {
   try {
+    console.log("getting the page to scrape");
     const { data } = await axios.get(
       "https://www.proteinreport.org/cultivated"
     );
     console.log(data);
-    const dom = new JSDOM(data, {
-      runScripts: "dangerously",
-      resources: "usable",
-    });
-    const { document } = dom.window;
-    const firstPost = document.querySelector("div.field-content > a").href;
+    const $ = cheerio.load(data);
+    const firstPost = $("div.field-content > a").attr("href");
     console.log("First post", firstPost);
+    return firstPost;
   } catch (error) {
     throw error;
   }
