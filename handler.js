8c18e6217a06f5249815a3416793fb0f98b1e1fe
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-11-12 16:15:57 +1100

    added response for slack; stable function

--- handler.js
+++ handler.js
@@ -1,53 +1,54 @@
 import { App } from "@slack/bolt";
 import { getFirstPost } from "./scraper";
 
+const success_response = {
+  statusCode: 200,
+  body: null,
+};
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+const messageResponse = async (slackE, callback) => {
+  //callback sending Slack a 200 response
+  callback(null, success_response);
+
+  //determining type of request from slack
+  if (slackE.bot_id == "B01E5B1LCMQ") {
+    console.log("I know I am a bot, return this");
+    return;
+  } else if (
+    slackE.text == "News" &&
+    slackE.channel_type == "im" &&
+    slackE.type == "message"
+  ) {
+    const article = await getFirstPost();
+    await publishMessage(slackE.channel, article);
+    return;
+  } else {
+    console.log("Nothing to report");
+    return;
+  }
+};
+
 async function publishMessage(id, text) {
   try {
-    // Call the chat.postMessage method using the built-in WebClient
     const result = await app.client.chat.postMessage({
-      // The token you used to initialize your app
       token: process.env.SLACK_BOT_TOKEN,
       channel: id,
       text: text,
-      // You could also use a blocks[] array to send richer content
     });
-
-    // Print result, which includes information about the message (like TS)
     console.log(result);
   } catch (error) {
     console.error(error);
   }
 }
 
-export const hello = async (event, context) => {
-  console.log("Received event:", JSON.stringify(event, null, 2));
+//the handler receiving the response
+export const hello = (event, context, callback) => {
   const body = JSON.parse(event.body);
-  console.log("Event Body:", body);
   const slackE = body.event;
-  console.log("this sis app", app);
-  app.action("message", async ({ action, ack }) => {
-    console.log("Acknowledging now");
-    await ack();
-    console.log("acknowledged");
-  });
-  // this isnt robust enough at the moment. I need it to handle non related commands and then end, rather than keep firing
-  if (slackE.bot_id == "B01E5B1LCMQ") {
-    return;
-  } else if (
-    slackE.text == "Newsbot send news" &&
-    slackE.channel_type == "im" &&
-    slackE.type == "message"
-  ) {
-    return;
-  } else {
-    console.log("Nothing to report");
-    const article = await getFirstPost();
-    await publishMessage(slackE.channel, article);
-    return;
-  }
+  messageResponse(slackE, callback);
 };
