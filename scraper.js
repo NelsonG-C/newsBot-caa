4b4268b9938735142e3752ff288bb1c7d3969a32
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-11-12 16:16:08 +1100

    cleaned up function

--- scraper.js
+++ scraper.js
@@ -3,14 +3,12 @@ import cheerio from "cheerio";
 
 export const getFirstPost = async () => {
   try {
-    console.log("getting the page to scrape");
+    //retrieving article
     const { data } = await axios.get(
       "https://www.proteinreport.org/cultivated"
     );
-    console.log(data);
     const $ = cheerio.load(data);
     const firstPost = $("div.field-content > a").attr("href");
-    console.log("First post", firstPost);
     return firstPost;
   } catch (error) {
     throw error;
